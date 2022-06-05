68ad0f9aaf2316051530c3ff326e69c8c90e3cce
Desiree Moya <desmp7@gmail.com>
2022-06-05 12:58:08 +0200

    Clase de prueba

---
+++ PruebaNotas2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaNotas2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaNotas2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3538568c-d6eb-4128-a62c-c1849f4fde0a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebaNotas2/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace PruebaNotas2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> notas = new List<int>();
+
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            double mediaEsperada = 5.143;
+            int susE = 3;
+            int aprE = 1;
+            int notE = 2;
+            int sbrE = 1;
+        }
+    }
+}
