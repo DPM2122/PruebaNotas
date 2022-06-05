8187f58ecf35986f6a45772e651ab833edef80b5
Desiree Moya <desmp7@gmail.com>
2022-06-05 15:38:52 +0200

    Resultado final

--- ProyectoNotas/estadisticasNotas.cs
+++ ProyectoNotas/estadisticasNotas.cs
@@ -9,7 +9,7 @@ namespace ProyectoNotas
     /// <summary>
     /// Clase que calcula las estadíaticas de un conjunto de notas
     /// </summary>
-    public class estadisticasNotas  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    public class EstadisticasNotas  // esta clase nos calcula las estadísticas de un conjunto de notas 
     {
         private int suspensos;  // Suspensos
         private int aprobados;  // Aprobados
@@ -17,17 +17,56 @@ namespace ProyectoNotas
         private int sobresalientes;  // Sobresalientes
 
         private double media; // Nota media
+
+        /// <summary>
+        /// Incluimos los getters y los setters de todas las variables
+        /// </summary>
+        public int NumeroSuspensos
+        {
+            get { return suspensos; }
+            set { suspensos = value; }
+        }
+        
+
+        public int NumeroAprobados
+        {
+            get { return aprobados; }
+            set { aprobados = value; }
+        }
+
+        public int NumeroNotables
+        {
+            get { return notables; }
+            set { notables = value; }
+        }
+
+        public int NumeroSobresalientes
+        {
+            get { return sobresalientes; }
+            set { sobresalientes = value; }
+        }
+        
+       
+        public double Media
+        {
+            get { return media; }
+            set { media = value; }
+        }
+
+
+
         /// <summary>
         /// Se innicializan las variables a 0
         /// </summary>
         // Constructor vacío
-        public estadisticasNotas()
+
+        public EstadisticasNotas()
         {
-            suspensos = 0;
-            aprobados = 0;
-            notables = 0;
-            sobresalientes = 0;  // inicializamos las variables
-            media = 0.0;
+            NumeroSuspensos = 0;
+            NumeroAprobados = 0;
+            NumeroNotables = 0;
+            NumeroSobresalientes = 0;  // inicializamos las variables
+            Media = 0.0;
         }
         /// <summary>
         /// Constuctor a partir de una lista al crear un objeto
@@ -35,30 +74,30 @@ namespace ProyectoNotas
         /// <param name="listaNotas">Lista con las notas de los alumnos</param>
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estadisticasNotas(List<int> listaNotas)
+        public EstadisticasNotas(List<int> listaNotas)
         {
             Estadisticas(listaNotas);
         }
 
         /// <summary>
-        /// Funcion para introducir notas
+        /// Clacula la media de las notas introducidas
         /// </summary>
         /// <param name="listaNotas">Lista de notas introducidas</param>
         private void Estadisticas(List<int> listaNotas)
         {
-            media = 0.0;
+            Media = 0.0;
 
             for (int i = 0; i < listaNotas.Count; i++)
             {
-                if (listaNotas[i] < 5) suspensos++;              // Por debajo de 5 suspenso
-                else if (listaNotas[i] > 5 && listaNotas[i] < 7) aprobados++;// Nota para aprobar: 5
-                else if (listaNotas[i] > 7 && listaNotas[i] < 9) notables++;// Nota para notable: 7
-                else if (listaNotas[i] > 9) sobresalientes++;         // Nota para sobresaliente: 9
+                if (listaNotas[i] < 5) NumeroSuspensos++;              // Por debajo de 5 suspenso
+                else if (listaNotas[i] >= 5 && listaNotas[i] < 7) NumeroAprobados++;// Nota para aprobar: 5
+                else if (listaNotas[i] >= 7 && listaNotas[i] < 9) NumeroNotables++;// Nota para notable: 7
+                else if (listaNotas[i] >= 9) NumeroSobresalientes++;         // Nota para sobresaliente: 9
 
-                media = media + listaNotas[i];
+                Media = Media + listaNotas[i];
             }
 
-            media = media / listaNotas.Count;
+            Media = Media / listaNotas.Count;
         }
 
         /// <summary>
@@ -66,6 +105,10 @@ namespace ProyectoNotas
         /// </summary>
         /// <param name="listaNotas">Lista con las notas de los alumnos</param>
         /// <returns>Devuelve la media y el numero de  notas</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Las notas están fuera del 
+        /// rango establecido</exception>
+        /// <exception cref="System.Exception">La lista de notas está vacía</exception>
+      
 
         // Para un conjunto de listnot, calculamos las estadísticas
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
@@ -73,22 +116,20 @@ namespace ProyectoNotas
         // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
         public double calcularEstadistica(List<int> listaNotas)
         {
-            media = 0.0;
+           
 
             // TODO: hay que modificar el tratamiento de errores para generar excepciones
             //
             if (listaNotas.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
                 throw new Exception("Lista vacía");
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < listaNotas.Count; i++)
                 if (listaNotas[i] < 0 || listaNotas[i] > 10)      // comprobamos que las notables están entre 0 y 10 (mínimo y máximo), si no, error
-                    throw new ArgumentOutOfRangeException("Nota ", i, " Nota fuera de rango válido");
+                    throw new ArgumentOutOfRangeException("Contador i ", i, " Nota fuera de rango válido");
 
             Estadisticas(listaNotas);
 
-            media = media / listaNotas.Count;
-
-            return media;
+            return Media;
         }
     }
 }
--- PruebaNotas2/UnitTest1.cs
+++ PruebaNotas2/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using ProyectoNotas;
 
 namespace PruebaNotas2
 {
@@ -8,25 +9,64 @@ namespace PruebaNotas2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaValoresCorrectos()
         {
-            List<int> notas = new List<int>();
+            EstadisticasNotas estadisticas = new EstadisticasNotas();
+            List<int> listaNotas = new List<int>();
 
-            notas.Add(0);
-            notas.Add(5);
-            notas.Add(9);
-            notas.Add(3);
-            notas.Add(7);
-            notas.Add(4);
-            notas.Add(8);
+            listaNotas.Add(0);
+            listaNotas.Add(5);
+            listaNotas.Add(9);
+            listaNotas.Add(3);
+            listaNotas.Add(7);
+            listaNotas.Add(4);
+            listaNotas.Add(8);
 
             double mediaEsperada = 5.143;
             int suspensosEsperados = 3;
             int aprobadosEsperados = 1;
             int notablesEsperados = 2;
             int sobresalientesEsperados = 1;
+           
+            
+            estadisticas.calcularEstadistica(listaNotas);
 
+            int numeroSuspendidosMetodo = estadisticas.NumeroSuspensos;
+            int numeroAprobadosMetodo = estadisticas.NumeroAprobados;
+            int numeroNotablesMetodo = estadisticas.NumeroNotables;
+            int numeroSobresalientesMetodo = estadisticas.NumeroSobresalientes;
+            double mediaMetodo = estadisticas.Media;
 
+            Assert.AreEqual(suspensosEsperados, numeroSuspendidosMetodo, 0.01, "El numero de suspensos no coincide");
+            Assert.AreEqual(aprobadosEsperados, numeroAprobadosMetodo, 0.01, "El numero de Aprobados no coincide");
+            Assert.AreEqual(notablesEsperados, numeroNotablesMetodo, 0.01, "El numero de Notables no coincide");
+            Assert.AreEqual(sobresalientesEsperados, numeroSobresalientesMetodo, 0.001, "El numero de Sobresalientes no coincide");
+            Assert.AreEqual(mediaEsperada, mediaMetodo, 0.01, "La media no coincide");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PruebaListaVacia_ExcepcionEsperada()
+        {
+            List<int> listaNotas = new List<int>();
+            EstadisticasNotas  estadisticas = new EstadisticasNotas();
+            estadisticas.calcularEstadistica(listaNotas);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PruebaNotasIncorrectas_ExcepcionEsperada()
+        {
+            List<int> listaNotas = new List<int>();
+            listaNotas.Add(0);
+            listaNotas.Add(5);
+            listaNotas.Add(-9);
+            listaNotas.Add(3);
+            listaNotas.Add(7);
+            listaNotas.Add(4);
+            listaNotas.Add(8);
+
+            EstadisticasNotas estadisticas = new EstadisticasNotas();
+            estadisticas.calcularEstadistica(listaNotas);
         }
     }
 }
